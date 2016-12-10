319be7d6f0bda893ea3d7d4f920f2b7c91d3958c
Дмитрий Мурзин <dm1998@list.ru>
2016-12-11 00:55:14 +0300

    Improve debug.h

--- debug.h
+++ debug.h
@@ -11,13 +11,12 @@ void nop() {}
 */
 bool dont = 0;
 
-
-
 #include <sstream>
 #include <iostream>
 #include <vector>
 #include <cstring>
 using namespace std;
+ostream &COUT = cout;
 
 template<typename Iterator>
 ostream &write(ostream &out, Iterator begin, Iterator end) {
@@ -31,14 +30,24 @@ ostream &write(ostream &out, Iterator begin, Iterator end) {
 	return out << '}';
 }
 
-template<template<typename...> typename C, typename... E, typename enable_if<!is_same<C<E...>, basic_string<char>>::value>::type * = nullptr>
+template<typename T1 = int, typename T2 = int, typename ...>
+struct getPairType {
+	typedef pair<T1, T2> type;
+};
+
+template<template<typename...> typename C, typename... E, typename enable_if<!is_same<C<E...>, string>::value && !is_same<C<E...>, typename getPairType<E...>::type>::value>::type * = nullptr>
 ostream &operator<<(ostream &out, C<E...> c) {
 	return write(out, c.begin(), c.end());
 }
 
-template<typename Iterator>
+template<typename Iterator, typename = decltype(*declval<Iterator &>(), void(), ++declval<Iterator &>(), void())>
 ostream &operator<<(ostream &out, pair<Iterator, Iterator> c) {
 	return write(out, c.first, c.second);
+}
+
+template<typename A, typename B>
+ostream &operator<<(ostream &out, pair<A, B> p) {
+	return out << '{' << p.first << ", " << p.second << '}';
 }
 
 template<typename Arg1>
@@ -61,19 +70,20 @@ void myPrint(const char *names, Args &&... args) {
 	if (dont) return;
 	ostringstream ss;
 	myPrint(ss, names, args...);
-	cerr << ss.str();
+	COUT << ss.str();
 
 	// or for sequential programms
-	// myPrint(cerr, names, args...);
+	// myPrint(COUT, names, args...);
 #endif
 }
 
-#define msg(s) cout << (s + std::string("\n"))
+#define msg(s) if (!dont) COUT << (s + std::string("\n"))
 #define dbgt(...) myPrintLabel("\t", #__VA_ARGS__, __VA_ARGS__)
 #define dbgtt(...) myPrintLabel("\t\t", #__VA_ARGS__, __VA_ARGS__)
 #define dbgttt(...) myPrintLabel("\t\t\t", #__VA_ARGS__, __VA_ARGS__)
 #define dbgtttt(...) myPrintLabel("\t\t\t\t", #__VA_ARGS__, __VA_ARGS__)
 #define dbgl(label, ...) myPrintLabel((label + string(": ")), #__VA_ARGS__, __VA_ARGS__)
+#define print_clock() printf("%.3f секунд\n", clock() / (float) CLOCKS_PER_SEC)
 
 template<typename... Args>
 void myPrintLabel(string label, const char *names, Args &&... args) {
@@ -81,6 +91,23 @@ void myPrintLabel(string label, const char *names, Args &&... args) {
 	if (dont) return;
 	ostringstream ss;
 	myPrint(ss, names, args...);
-	cerr << label + ss.str();
+	COUT << label + ss.str();
 #endif
+}
+
+template<typename T>
+void test(T t, string expected) {
+	ostringstream out;
+	out << t;
+	assert(out.str() == expected);
+}
+
+void test() {
+	test(vector<int>{1, 2, 3}, "{1, 2, 3}");
+	test(set<int>{1, 2, 3}, "{1, 2, 3}");
+	test(map<int, int>{{1, 10},
+	                   {2, 20}}, "{{1, 10}, {2, 20}}");
+	test(pair<int, int>{1, 2}, "{1, 2}");
+	test(string{"abc"}, "abc");
+	test("abc", "abc");
 }
