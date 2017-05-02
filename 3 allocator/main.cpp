eaaa6cd34b94d9cbd57cd011350ac97707e5ad94
Дмитрий Мурзин <dm1998@list.ru>
2017-05-02 08:17:20 +0300

    Утечки памяти

--- 3 allocator/main.cpp
+++ 3 allocator/main.cpp
@@ -4,169 +4,236 @@ using namespace std;
 #include "/home/dima/C++/debug.h"
 #endif
 
-struct BlockInfo {
-    void *superblock;
-    int threadIndex;
-    int blockType;
+struct Superblock;
+
+struct Block {
+	Superblock *superblock;
+	int threadIndex;
+	int blockType;
+
+	char *getBlockData();
+
+	Block *getBlockAfterThis(int blockType);
+
+	static Block *getBlockFromData(char *pointer);
+
+	int getDataSize();
+};
+
+struct Superblock {
+	int dataSize;
+
+	char *getSuperblockData();
+
+	Block *getLargestBlock();
+
+	Block *getKthMinblock(int k);
 };
 
 int log2i(size_t size) {
-    int log = 0;
-    while (size > 1) {
-        size /= 2;
-        ++log;
-    }
-    return log;
+	int log = 0;
+	while (size > 1) {
+		size /= 2;
+		++log;
+	}
+	return log;
 }
 
 const int NUMBER_MINBLOCKS_IN_SUPERBLOCK = 8 * 1024;
 const int MINBLOCK_SIZE_DATA = 128;
-const int MINBLOCK_SIZE_ALL = MINBLOCK_SIZE_DATA + sizeof(BlockInfo);
+const int MINBLOCK_SIZE_ALL = MINBLOCK_SIZE_DATA + sizeof(Block);
 const int SUPERBLOCK_SIZE_DATA = NUMBER_MINBLOCKS_IN_SUPERBLOCK * MINBLOCK_SIZE_DATA;
 const int SUPERBLOCK_SIZE_ALL = NUMBER_MINBLOCKS_IN_SUPERBLOCK * MINBLOCK_SIZE_ALL;
 const int NUMBER_BLOCK_TYPES = log2i(MINBLOCK_SIZE_DATA * NUMBER_MINBLOCKS_IN_SUPERBLOCK) + 1;
 const int BLOCK_TYPE_MIN = log2i(MINBLOCK_SIZE_DATA);
 
+Block *Block::getBlockAfterThis(int blockType) {
+	return (Block *) ((char *) this + (1 << blockType) + sizeof(Block) * (1 << (blockType - BLOCK_TYPE_MIN)));
+}
+
+char *Block::getBlockData() {
+	return (char *) this + sizeof(Block);
+}
+
+Block *Block::getBlockFromData(char *data) {
+	return (Block *) (data - sizeof(Block));
+}
+
+int Block::getDataSize() {
+	return 1 << blockType;
+}
+
+char *Superblock::getSuperblockData() {
+	return (char *) this + sizeof(Superblock);
+}
+
+Block *Superblock::getKthMinblock(int k) {
+	return (Block *) (getSuperblockData() + k * MINBLOCK_SIZE_ALL);
+}
+
+Block *Superblock::getLargestBlock() {
+	return (Block *) getSuperblockData();
+}
+
 struct GlobalAllocator {
 //    суперблок --- несколько последовательных минблоков
-//    минблок --- <BlockInfo> и <MINBLOCK_SIZE_DATA байт>
-    vector<void *> superblocks;
+//    минблок --- <Block> и <MINBLOCK_SIZE_DATA байт>
+	vector<Superblock *> superblocks;
 
-    void *getSuperBlock() {
-        void *superblock = malloc(SUPERBLOCK_SIZE_ALL);
-        superblocks.push_back(superblock);
-        return superblock;
-    }
+	Superblock *getSuperBlock() {
+		assert(sizeof(char) == 1);
+		Superblock *superblock = (Superblock *) malloc(sizeof(Superblock) + SUPERBLOCK_SIZE_ALL);
+		superblock->dataSize = 0;
+		superblocks.push_back(superblock);
+		return superblock;
+	}
+
+	~GlobalAllocator() {
+		for (Superblock *superblock : superblocks) {
+			assert(superblock->dataSize == 0);
+			free(superblock);
+		}
+	}
 };
 
 GlobalAllocator globalAllocator;
 mutex globalAllocatorMutex;
 
 struct LocalAllocator {
-    int threadIndex;
-    vector<void *> superblocks;
-    vector<std::list<void *>> blocksBySize;
+	int threadIndex;
+	vector<Superblock *> superblocks;
+	vector<std::list<Block *>> blocksBySize;
 
-    LocalAllocator() : blocksBySize(NUMBER_BLOCK_TYPES) {
-        static atomic_int numberLocalAllocators;
-        threadIndex = numberLocalAllocators++;
-    }
+	LocalAllocator() : blocksBySize(NUMBER_BLOCK_TYPES) {
+		static atomic_int numberLocalAllocators;
+		threadIndex = numberLocalAllocators++;
+	}
 
-    void *getSuperBlock() {
-        dbgl("getSuperBlock", threadIndex);
-        void *superblock = globalAllocator.getSuperBlock();
-        for (int i = 0; i < NUMBER_MINBLOCKS_IN_SUPERBLOCK; ++i) {
-            void *block = superblock + i * MINBLOCK_SIZE_ALL;
-            BlockInfo &blockInfo = *((BlockInfo *) block);
-            blockInfo.threadIndex = threadIndex;
-            blockInfo.superblock = block;
-        }
-        return superblock;
-    }
+	Superblock *getSuperBlock() {
+		dbgl("getSuperBlock", threadIndex);
+		Superblock *superblock = globalAllocator.getSuperBlock();
+		for (int i = 0; i < NUMBER_MINBLOCKS_IN_SUPERBLOCK; ++i) {
+			Block *block = superblock->getKthMinblock(i);
+			block->threadIndex = threadIndex;
+			block->superblock = superblock;
+		}
+		superblocks.push_back(superblock);
+		return superblock;
+	}
 
-    void *mtalloc(size_t size) {
-        assert(size <= MINBLOCK_SIZE_DATA * NUMBER_MINBLOCKS_IN_SUPERBLOCK);
-        void *block = getBlock(size);
-        if (block == nullptr) {
-            // блок равного или большего размера не нашёлся
-            lock_guard<mutex> lock(globalAllocatorMutex);
-            void *superBlock = getSuperBlock();
-            blocksBySize.back().push_back(superBlock);
-            block = getBlock(size);
-        }
+	char *mtalloc(size_t size) {
+		assert(size <= MINBLOCK_SIZE_DATA * NUMBER_MINBLOCKS_IN_SUPERBLOCK);
+		Block *block = getBlock(size);
+		if (block == nullptr) {
+			// блок равного или большего размера не нашёлся
+			lock_guard<mutex> lock(globalAllocatorMutex);
+			Superblock *superblock = getSuperBlock();
+			blocksBySize.back().push_back(superblock->getLargestBlock());
+			block = getBlock(size);
+		}
+		dbgl("+", threadIndex, size, block);
 
-        dbgl("+", threadIndex, size, block);
-        BlockInfo &blockInfo = *((BlockInfo *) block);
-        assert(blockInfo.threadIndex == threadIndex);
-        return block + sizeof(BlockInfo);
-    }
+		Superblock *superblock = block->superblock;
+		superblock->dataSize += block->getDataSize();
+		return block->getBlockData();
+	}
 
-    int divideBlockToFitSize(void *block, int blockType, size_t size) {
+	int divideBlockToFitSize(Block *block, int blockType, size_t size) {
 //        пока блое не является минблоком и размер данных укладывается в вдвое меньший блок
-        while (blockType > BLOCK_TYPE_MIN && size <= (1 << blockType) / 2) {
+		while (blockType > BLOCK_TYPE_MIN && size <= (1 << blockType) / 2) {
 //            делим блок на две части
-            --blockType;
-            blocksBySize[blockType].push_back(block + (1 << blockType) + sizeof(BlockInfo) * (1 << (blockType - BLOCK_TYPE_MIN)));
-        }
-        return blockType;
-    }
+			--blockType;
+			blocksBySize[blockType].push_back(block->getBlockAfterThis(blockType));
+		}
+		return blockType;
+	}
 
-    void *getBlock(size_t size) {
-        for (int i = BLOCK_TYPE_MIN; i < NUMBER_BLOCK_TYPES; ++i) {
-            int blockSize = 1 << i;
-            if (size <= blockSize && !blocksBySize[i].empty()) {
-                void *block = blocksBySize[i].back();
-                blocksBySize[i].pop_back();
-                BlockInfo &blockInfo = *((BlockInfo *) block);
-                blockInfo.blockType = divideBlockToFitSize(block, i, size);
-                return block;
-            }
-        }
-        return nullptr;
-    }
+	Block *getBlock(size_t size) {
+		for (int i = BLOCK_TYPE_MIN; i < NUMBER_BLOCK_TYPES; ++i) {
+			int blockSize = 1 << i;
+			if (size <= blockSize && !blocksBySize[i].empty()) {
+				Block *block = blocksBySize[i].back();
+				blocksBySize[i].pop_back();
+				block->blockType = divideBlockToFitSize(block, i, size);
+				block->threadIndex = threadIndex;
+				return block;
+			}
+		}
+		return nullptr;
+	}
 
-    void mtfree(void *pointer) {
-        dbgl("-", threadIndex, pointer);
-        void *block = pointer - sizeof(BlockInfo);
-        BlockInfo &blockInfo = *((BlockInfo *) block);
-        if (blockInfo.threadIndex == threadIndex) {
-            blocksBySize[blockInfo.blockType].push_back(block);
-        } else {
-            assert(false);
-        }
-    }
+	void mtfree(char *data) {
+		dbgl("-", threadIndex, data);
+		Block *block = Block::getBlockFromData(data);
+		if (block->threadIndex == threadIndex) {
+			blocksBySize[block->blockType].push_back(block);
+			Superblock *superblock = block->superblock;
+			superblock->dataSize -= block->getDataSize();
+		} else {
+			assert(false);
+		}
+	}
 };
 
 LocalAllocator &getLocalAllocator() {
-    static thread_local LocalAllocator allocator;
-    return allocator;
+	static thread_local LocalAllocator allocator;
+	return allocator;
 }
 
 extern void *mtalloc(size_t size) {
-    return getLocalAllocator().mtalloc(size);
+	return getLocalAllocator().mtalloc(size);
 }
 
-extern void mtfree(void *pointer) {
-    return getLocalAllocator().mtfree(pointer);
+extern void mtfree(void *data) {
+	return getLocalAllocator().mtfree((char *) data);
 }
 
 #ifdef LOCAL
 
-int number_threads = 10;
-int number_operations = 100;
+void thread_function(int numberOperations, bool useCustomMalloc) {
+	void *(*mallocUsed)(size_t);
+	void (*freeUsed)(void *);
+	if (useCustomMalloc) {
+		mallocUsed = mtalloc;
+		freeUsed = mtfree;
+	} else {
+		mallocUsed = malloc;
+		freeUsed = free;
+	}
 
-void thread_function() {
-    vector<void *> datas;
-    for (int iOperation = 0; iOperation < number_operations; ++iOperation) {
-        if (datas.empty() || rand() % 2 == 0) {
-            datas.push_back(mtalloc(rand() % SUPERBLOCK_SIZE_DATA + 1));
-        } else {
-            int i = rand() % datas.size();
-            mtfree(datas[i]);
-            datas[i] = datas.back();
-            datas.pop_back();
-        }
-    }
+	const int maxBlockSize = 1024 * 16;
+	vector<void *> datas;
+	for (int iOperation = 0; iOperation < numberOperations; ++iOperation) {
+		if (datas.empty() || rand() % 2 == 0) {
+			datas.push_back(mallocUsed(rand() % maxBlockSize + 1));
+		} else {
+			int i = rand() % datas.size();
+			freeUsed(datas[i]);
+			datas[i] = datas.back();
+			datas.pop_back();
+		}
+	}
 
-    for (void *&data : datas) {
-        mtfree(data);
-    }
+	for (void *&data : datas) {
+		freeUsed(data);
+	}
 }
 
-void test() {
-    vector<thread> threads;
-    for (int i = 0; i < number_threads; ++i) {
-        threads.emplace_back(thread_function);
-    }
+void test(int numberThreads, int numberOperations, bool useCustomMalloc) {
+	vector<thread> threads;
+	for (int i = 0; i < numberThreads; ++i) {
+		threads.emplace_back(thread_function, numberOperations, useCustomMalloc);
+	}
 
-    for (thread &t : threads) {
-        t.join();
-    }
+	for (thread &t : threads) {
+		t.join();
+	}
 }
 
 int main() {
-    test();
-    return 0;
+	test(10, 100, true);
+//	test(10, 100, false);
+	return 0;
 }
 
 #endif
